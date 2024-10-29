dbdc1a319f7544536314a6f864960034dc8186af
greaneagle <greaneagle@gmail.com>
2024-10-29 13:49:47 +0000

    First Prep test

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,6 @@
 const admin = require('firebase-admin');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 admin.initializeApp();
 const config = require('./config.json');
 const functions = require('firebase-functions');
-
-let fireactjsSaasFunctions =  require('./lib')({config: config, admin: admin, functions: functions});
-
+let fireactjsSaasFunctions =  require('@fireactjs/saas-cloud-functions')({config: config, admin: admin, functions: functions});
 exports.fireactjsSaas = fireactjsSaasFunctions;
